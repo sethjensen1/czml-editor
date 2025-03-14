718ad5854e1636655615fd895fd64fbaa9877d49
dkiselev <dmitry.v.kiselev@gmail.com>
2025-03-14 10:42:41 -0300

    Fix label metadata, fix feature name

--- src/editor/meta/label-meta.ts
+++ src/editor/meta/label-meta.ts
@@ -1,11 +1,12 @@
 import { PropertyMeta } from "./meta";
 
 const appearanceProps: PropertyMeta[] = [
+    {name: 'show', type: 'boolean'},
     {name: 'text', type: 'string'},
 ];
 
 export const labelMetadata = {
-    feature: 'billboard',
+    feature: 'label',
     propertyGroups: [{
         title: 'Text and Font',
         properties: appearanceProps
